10730ddfe8da2b18182ecb100dade564655456c5
Elle Szabo <eszabo@usc.edu>
2023-02-23 02:22:07 -0800

    thing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h5>Designed and Developed by Soumyajit Behera</h5>
+          <h5>based off of Soumyajit Behera</h5>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
